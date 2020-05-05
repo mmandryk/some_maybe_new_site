ed4a2801598a506712a09e71fe9e54e4ccf2aa27
Marcin Mandryk <js.mandrykmarcin@gmail.com>
2020-05-05 18:04:43 +0200

    Updates

---
+++ precache-manifest.97664d3cbc472bbef73537b3f887e4eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55b4fac5e6486058221ea61dbbdd9f5",
+    "url": "/some_maybe_new_site/index.html"
+  },
+  {
+    "revision": "1e375cb641c50ce7cd3c",
+    "url": "/some_maybe_new_site/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "035320b17230a693df0b",
+    "url": "/some_maybe_new_site/static/js/2.aedb7138.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/some_maybe_new_site/static/js/2.aedb7138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e375cb641c50ce7cd3c",
+    "url": "/some_maybe_new_site/static/js/main.6a3d274f.chunk.js"
+  },
+  {
+    "revision": "3a17642aad71d10a3343",
+    "url": "/some_maybe_new_site/static/js/runtime-main.1914bc83.js"
+  },
+  {
+    "revision": "956ee53a5560ecb6860599d987f684ca",
+    "url": "/some_maybe_new_site/static/media/1.956ee53a.jpg"
+  },
+  {
+    "revision": "622eb964298b2216f8274486ce402c47",
+    "url": "/some_maybe_new_site/static/media/2.622eb964.jpg"
+  },
+  {
+    "revision": "0b08aa00397ed444bced87aa6fd2ce41",
+    "url": "/some_maybe_new_site/static/media/3.0b08aa00.jpg"
+  },
+  {
+    "revision": "4d0eda291d4568c8a2242fa9f3c60bd3",
+    "url": "/some_maybe_new_site/static/media/4.4d0eda29.jpg"
+  },
+  {
+    "revision": "e32dff2bec7b21ad5d86a33a3d6210bb",
+    "url": "/some_maybe_new_site/static/media/jumbotron2.e32dff2b.jpg"
+  },
+  {
+    "revision": "7c611a8fd099d68cb253337c0e6424b2",
+    "url": "/some_maybe_new_site/static/media/tree.7c611a8f.jpg"
+  }
+]);
